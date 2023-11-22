2d41dce17e121c64343be2f7066d5d5b0a1d62f2
Grogro21 <74548874+Grogro21@users.noreply.github.com>
2023-11-22 13:28:04 +0100

    battle pictures
    code pour superposer des images en combat

--- Plugins/Mid Battle Scripts/utilities.rb
+++ Plugins/Mid Battle Scripts/utilities.rb
@@ -67,5 +67,118 @@ def throwitem(pkmn)
 end
 
 
+##########Battle pictures######################################
 
+class Battle::Scene
+  def appearsprite(spritenames)
+    pbAddSprite("bob",Graphics.width,0,"Graphics/Battle animations/pictures/"+spritename1,@viewport)
+	pbAddSprite("bob2",Graphics.width,0,"Graphics/Battle animations/pictures/"+spritename2,@viewport)
+	pbAddSprite("bob3",Graphics.width,0,"Graphics/Battle animations/pictures/"+spritename2,@viewport)
+	pbAddSprite("bob4",Graphics.width,0,"Graphics/Battle animations/pictures/"+spritename2,@viewport)
+	pbAddSprite("bob5",Graphics.width,0,"Graphics/Battle animations/pictures/"+spritename2,@viewport)
+    unfadeAnim = SpriteAppearAnimation.new(@sprites,@viewport,@battle.battlers.length)
+    @animations.push(unfadeAnim)
+    loop do
+      unfadeAnim.update
+      pbUpdate
+      break if unfadeAnim.animDone?
+    end
+    unfadeAnim.dispose
+  end
+  
+  def disappearsprite(spritename1,spritename2)
+    unfadeAnim = SpriteDisappearAnimation.new(@sprites,@viewport,@battle.battlers.length)
+    @animations.push(unfadeAnim)
+    loop do
+      unfadeAnim.update
+      pbUpdate
+      break if unfadeAnim.animDone?
+    end
+    unfadeAnim.dispose
+  end
+end
 
+class SpriteAppearAnimation < Battle::Scene::Animation
+  def initialize(sprites,viewport,battlers)
+    @battlers = battlers
+    super(sprites,viewport)
+  end
+
+  def createProcesses
+    delay = 10
+    boxes = []
+    toMoveTop = [@sprites["bob"].bitmap.width,Graphics.width].max
+    topBar = addSprite(@sprites["bob"],PictureOrigin::TOP_LEFT)
+    topBar.setZ(0,200)
+    topBar.setOpacity(0,255)
+    topBar.setXY(0,Graphics.width,0)
+    topBar.moveXY(delay,10,(Graphics.width-toMoveTop),0)
+	
+	toMoveTop2 = [@sprites["bob2"].bitmap.width,Graphics.width].max
+    topBar2 = addSprite(@sprites["bob2"],PictureOrigin::TOP_LEFT)
+    topBar2.setZ(0,200)
+    topBar2.setOpacity(0,255)
+    topBar2.setXY(0,Graphics.width,0)
+    topBar2.moveXY(delay,10,(Graphics.width-toMoveTop2),0)
+	
+	toMoveTop3 = [@sprites["bob3"].bitmap.width,Graphics.width].max
+    topBar3 = addSprite(@sprites["bob3"],PictureOrigin::TOP_LEFT)
+    topBar3.setZ(0,200)
+    topBar3.setOpacity(0,255)
+    topBar3.setXY(0,Graphics.width,0)
+    topBar3.moveXY(delay,10,(Graphics.width-toMoveTop2),0)
+	
+	toMoveTop4 = [@sprites["bob4"].bitmap.width,Graphics.width].max
+    topBar4 = addSprite(@sprites["bob4"],PictureOrigin::TOP_LEFT)
+    topBar4.setZ(0,200)
+    topBar4.setOpacity(0,255)
+    topBar4.setXY(0,Graphics.width,0)
+    topBar4.moveXY(delay,10,(Graphics.width-toMoveTop2),0)
+	
+	toMoveTop5 = [@sprites["bob5"].bitmap.width,Graphics.width].max
+    topBar5 = addSprite(@sprites["bob5"],PictureOrigin::TOP_LEFT)
+    topBar5.setZ(0,200)
+    topBar5.setOpacity(0,255)
+    topBar5.setXY(0,Graphics.width,0)
+    topBar5.moveXY(delay,10,(Graphics.width-toMoveTop2),0)
+	
+    for i in 0...@battlers
+      if @sprites["dataBox_#{i}"]
+        boxes[i]= addSprite(@sprites["dataBox_#{i}"])
+        boxes[i].moveOpacity(delay,5,0)
+      end
+    end
+  end
+end
+
+class SpriteDisappearAnimation < Battle::Scene::Animation
+  def initialize(sprites,viewport,battlers)
+    @battlers = battlers
+    super(sprites,viewport)
+  end
+
+  def createProcesses
+    delay = 10
+    boxes = []
+    topBar = addSprite(@sprites["bob"],PictureOrigin::TOP_LEFT)
+    topBar.setZ(0,200)
+    topBar = addSprite(@sprites["bob2"],PictureOrigin::TOP_LEFT)
+    topBar.setZ(0,200)
+	topBar = addSprite(@sprites["bob3"],PictureOrigin::TOP_LEFT)
+    topBar.setZ(0,200)
+	topBar = addSprite(@sprites["bob4"],PictureOrigin::TOP_LEFT)
+    topBar.setZ(0,200)
+	topBar = addSprite(@sprites["bob5"],PictureOrigin::TOP_LEFT)
+    topBar.setZ(0,200)
+    topBar.moveOpacity(delay,8,0)
+    topBar.moveOpacity(delay,8,0)
+    for i in 0...@battlers
+      if @sprites["dataBox_#{i}"]
+        boxes[i]= addSprite(@sprites["dataBox_#{i}"])
+        boxes[i].setOpacity(0,0)
+        boxes[i].moveOpacity(delay,5,255)
+      end
+    end
+    topBar.setXY(delay+5,Graphics.width,0)
+  end
+end
